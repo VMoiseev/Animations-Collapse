2d1dad043b150df1f566509d12d1ca720e1f2282
VMoiseev <e175@inbox.ru>
2023-02-07 01:41:22 +0500

    completed homework

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 // TODO: write code here
 
 console.log('app.js bundled');
+
+const btn = document.querySelector('.collapse');
+const collapse_container = document.querySelector('.foldingField');
+
+btn.addEventListener('click', () => {
+  collapse_container.classList.toggle('hidden');
+  collapse_container.classList.toggle('collapse_container');
+});
